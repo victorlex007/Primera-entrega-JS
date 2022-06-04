4e01f97fdd914c88210e34731cb8df08bf6ec2dc
victorlex007 <103323257+victorlex007@users.noreply.github.com>
2022-06-04 20:24:55 -0300

    Add files via upload

---
+++ Primera entrega JS/objetos.js
@@ -0,0 +1,11 @@
+const UMBRAL = 10
+const IVALOCAL = 1.21
+const IVAIMPORTADO = 1.105
+
+
+const producto1 = new Producto("CHORIZOS", 10, 1500, IVALOCAL)
+const producto2 = new Producto("VACIO", 15, 2500, IVALOCAL)
+const producto3 = new Producto("LOMO", 6, 2500, IVALOCAL)
+const producto4 = new Producto("STEAK", 4, 2000, IVAIMPORTADO)
+const producto5 = new Producto("TOMAHAWK", 10, 3000, IVAIMPORTADO)
+const producto6 = new Producto("T-BONE", 6, 1500, IVALOCAL)
